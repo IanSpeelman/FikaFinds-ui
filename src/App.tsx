3c33890e96858066c0e7b4f84f3c8fa2aa44f6d8
Ian Speelman <ianspeelman@gmail.com>
2024-10-14 19:15:06 +0200

    Header design done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
+import Layout from "./Pages/Layout/index.tsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    <>Hi there</>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />} >
+                    <Route path="/" element={<h1>hi there</h1>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
   )
 }
 
