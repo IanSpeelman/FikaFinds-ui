c9ca61e01800b0f7669a697ff3f0fbf521faf3dc
Ian Speelman <ianspeelman@gmail.com>
2024-10-14 20:48:22 +0200

    add product component

---
+++ src/Components/Product/index.tsx
@@ -0,0 +1,20 @@
+import styles from './index.module.css'
+
+type ProductProps = {
+    product: string,
+    image: string,
+    price: string,
+    category: string
+}
+
+
+export default function Product({ product, image, price, category }: ProductProps) {
+    return (
+        <a href='#' className={styles.container}>
+            <img className={styles.image} src={image} alt={product} />
+            <p className={styles.category}>{category}</p>
+            <p className={styles.product}>{product}</p>
+            <p className={styles.price}>{price}</p>
+        </a>
+    )
+}
