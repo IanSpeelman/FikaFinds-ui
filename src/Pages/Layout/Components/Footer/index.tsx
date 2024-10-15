cd46780cb5e4c20f4418b4459b518806a2581da6
Ian Speelman <ianspeelman@gmail.com>
2024-10-15 14:10:45 +0200

    Add footer

---
+++ src/Pages/Layout/Components/Footer/index.tsx
@@ -0,0 +1,21 @@
+import styles from './index.module.css'
+
+
+export default function Footer() {
+    return (
+        <header>
+            <div className={styles.container}>
+                <nav className={styles.nav}>
+                    <ul className={styles.ul}>
+                        <li className={styles.li}><a className={styles.link} href='#'>HOME</a></li>
+                        <li className={styles.li}><a className={styles.link} href='#'>ABOUT</a></li>
+                        <li className={styles.li}><a className={styles.link} href='#'>SHOP</a></li>
+                        <li className={styles.li}><a className={styles.link} href='#'>CONTACT</a></li>
+                    </ul>
+                </nav>
+                <h1 className={styles.title}>FIKA FINDS</h1>
+                <p>Copyright © 2024 Fika Finds</p>
+            </div>
+        </header>
+    )
+}
