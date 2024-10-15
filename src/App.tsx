e18f9ebc87ddc2637ef35ad9dd7a30bc4b0f9892
Ian Speelman <ianspeelman@gmail.com>
2024-10-15 18:35:27 +0200

    Add Contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import About from './Pages/About/index.tsx'
+import Contact from './Pages/Contact/index.tsx'
 import Home from './Pages/Home/index.tsx'
 import Layout from "./Pages/Layout/index.tsx"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
@@ -12,6 +13,7 @@ function App() {
                 <Route element={<Layout />} >
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Route>
             </Routes>
         </BrowserRouter>
