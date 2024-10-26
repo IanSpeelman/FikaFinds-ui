7834c524b99a478b110ca335599a935a3062da55
Ian Speelman <ianspeelman@gmail.com>
2024-10-26 19:40:31 +0200

    fix test

--- src/Components/Product/Product.test.tsx
+++ src/Components/Product/Product.test.tsx
@@ -2,13 +2,14 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom"
 import Product from "."
 
+
 describe("Product component", () => {
     it("should render with all the properties", () => {
-        const { container } = render(<Product product="product" price="price" category="category" image="image" />)
+        const { container } = render(<Product product={{ id: 1, name: "product", price: 5, category: "category", image: "image" }} />)
 
 
         const product = screen.getByText("product")
-        const price = screen.getByText("price")
+        const price = screen.getByText(5)
         const image = container.querySelector("img")
         const src = image!.getAttribute("src")
         const category = screen.getByText("category")
