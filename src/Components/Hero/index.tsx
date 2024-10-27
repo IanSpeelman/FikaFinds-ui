a58710f70ff3ecf6130ba8fcec71febba6f60cfe
Ian Speelman <ianspeelman@gmail.com>
2024-10-27 12:37:31 +0100

    Add small Hero size

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -1,7 +1,7 @@
 import styles from './index.module.css'
 
 type HeroProps = {
-    size?: 'medium',
+    size?: 'medium' | 'small',
     header: string,
     subheader?: string,
     buttonText?: string
@@ -10,7 +10,7 @@ type HeroProps = {
 
 export default function Hero({ size, header, subheader, buttonText, action }: HeroProps) {
     return (
-        <div className={`${styles.container} ${size === 'medium' ? styles.containerMedium : styles.containerLarge}`}>
+        <div className={`${styles.container} ${size ? styles[`container${size}`] : styles.containerlarge}`}>
             <div className={styles.content}>
                 <h2 className={styles.header}>{header}</h2>
                 {subheader && <h3 className={styles.subheader}>{subheader}</h3>}
