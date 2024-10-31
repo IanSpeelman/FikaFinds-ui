937c03f8769bd16616a6df0c0944b638a80dfaf3
Ian Speelman <ianspeelman@gmail.com>
2024-10-31 19:49:13 +0100

    change isValidPassword function
    refactored the isValidPassword function for clarity

--- src/utils/CheckCredentials.ts
+++ src/utils/CheckCredentials.ts
@@ -1,20 +1,22 @@
-export function isValidPassword(password: string): boolean {
+export function isValidPassword(inputPassword: string): boolean {
+    const password = inputPassword || ""
 
     const length = password.length > 7
     const capitals = password.match(/[A-Z]/g) !== null
     const lowerCase = password.match(/[a-z]/g) !== null
     const numbers = password.match(/[0-9]/g) !== null
-    const characters = (password.match(/[a-zA-Z0-9]/g)?.length ?? password.length) < password.length
 
+    let characterCount = password.match(/[a-zA-Z0-9]/g)?.length || 0
+    const characters = characterCount < password.length
 
     return length && capitals && lowerCase && numbers && characters
 }
-export function isValidEmail(email: string): boolean {
 
-    return email.match(/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/) !== null
 
+export function isValidEmail(email: string | null): boolean {
+    return email?.match(/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/) !== null
 }
 
-export function equalStrings(string1: string, string2: string) {
+export function equalStrings(string1: string | null, string2: string | null) {
     return string1 === string2
 }
