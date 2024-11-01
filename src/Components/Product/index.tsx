32aff6cae544148a4b4ea7e1e9bba34b7ed0c172
Ian Speelman <ianspeelman@gmail.com>
2024-11-01 16:18:26 +0100

    link the product on the product page to its show page
    also change the a tag to the React-router Link tag

--- src/Components/Product/index.tsx
+++ src/Components/Product/index.tsx
@@ -1,4 +1,5 @@
 import { product } from '../../utils/types'
+import { Link } from 'react-router-dom'
 import styles from './index.module.css'
 
 type ProductProps = {
@@ -8,13 +9,13 @@ type ProductProps = {
 
 export default function Product({ product }: ProductProps) {
     return (
-        <a href='#' className={styles.container}>
+        <Link to={`/show/${product.id}`} className={styles.container}>
             <img className={styles.image} src={product.image === "test" ? "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQhik6ntKzUrb7GbmRt0laNt4TDv7p8sdfywfGt9JLMf_ZDrvuJWV6o_v4yFe42PP-luPrrAe-jl7Kg9IYPsLVEbHDvViAcW7CnyQeUWkF52g_NoB7o0dU68g" : product.image} alt={product.name} />
             <div className={styles.text}>
                 <p className={styles.category}>{product.category}</p>
                 <p className={styles.product}>{product.name}</p>
                 <p className={styles.price}>{product.price}</p>
             </div>
-        </a>
+        </Link>
     )
 }
