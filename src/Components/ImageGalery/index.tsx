df0bad351c8fa5d01a0cd067450c7773b3b118a0
Ian Speelman <ianspeelman@gmail.com>
2024-11-04 10:03:14 +0100

    switching images
    switching images now works by changing the order of the src items in the
    array

--- src/Components/ImageGalery/index.tsx
+++ src/Components/ImageGalery/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import parseImageUrl from '../../utils/parseImageUrl'
 import { product } from '../../utils/types'
 import styles from './index.module.css'
@@ -8,13 +9,26 @@ type ImageGaleryProps = {
 
 export default function ImageGalery({ product }: ImageGaleryProps) {
 
-    const images = parseImageUrl(product.image).slice(0, 5)
+    const imageArray = parseImageUrl(product.image).slice(0, 5)
+    const [images, setImages] = useState(imageArray)
+
+    function switchImage(e: React.MouseEvent<HTMLImageElement>) {
+        const src = e.currentTarget.src
+        if (src !== images[0]) {
+            const index = images.findIndex(item => item === src)
+            let newArray = [...images]
+            newArray.splice(index, 1)
+            newArray.splice(0, 0, src)
+            setImages(newArray)
+        }
+    }
+
 
 
     return (<div className={styles.container}>
         <img src={images[0]} alt={product.name} className={styles.image} />
         <div className={styles.imageselector}>
-            {images.length > 1 && images.map(image => <img src={image} alt={product.name} className={styles.handle} />)}
+            {images.length > 1 && imageArray.map(image => <img src={image} key={Math.random()} onClick={(e) => switchImage(e)} alt={product.name} className={styles.handle} />)}
         </div>
     </div>
     )
