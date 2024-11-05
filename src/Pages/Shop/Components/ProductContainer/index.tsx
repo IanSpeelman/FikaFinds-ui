0f6d5ed7ca2121d0ad260bd290519f024c2a4f03
Ian Speelman <ianspeelman@gmail.com>
2024-11-05 14:39:14 +0100

    refactor price sort
    make sure the components load in the same order after component
    re-render

--- src/Pages/Shop/Components/ProductContainer/index.tsx
+++ src/Pages/Shop/Components/ProductContainer/index.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction } from "react"
+import { SetStateAction, useEffect, useRef } from "react"
 import Product from "../../../../Components/Product"
 import { product } from "../../../../utils/types"
 import styles from './index.module.css'
@@ -10,31 +10,43 @@ type ProductContainerProps = {
 
 export default function ProductContainer({ products, setProducts }: ProductContainerProps) {
 
+    const select = useRef<HTMLSelectElement | null>(null)
 
-    function handleChange(e: React.FormEvent<HTMLSelectElement>) {
-        if (e.currentTarget.value === 'priceasc') {
-            const sortedProducts = [...products];
-            sortedProducts.sort((x, y) => x.price - y.price)
-            setProducts(sortedProducts)
 
-        }
-        else if (e.currentTarget.value === 'pricedesc') {
-            const sortedProducts = [...products];
-            console.log('desc')
-            sortedProducts.sort((x, y) => y.price - x.price)
-            setProducts(sortedProducts)
-        }
+    function sortAsc() {
+        const sortedProducts = [...products];
+        sortedProducts.sort((x, y) => x.price - y.price)
+        setProducts(sortedProducts)
     }
 
+    function sortDesc() {
+        const sortedProducts = [...products];
+        sortedProducts.sort((x, y) => y.price - x.price)
+        setProducts(sortedProducts)
+    }
+
+
+    function handleChange(e: React.FormEvent<HTMLSelectElement>) {
+        e.currentTarget.value === 'priceasc' && sortAsc()
+        e.currentTarget.value === 'pricedesc' && sortDesc()
+    }
+
+    useEffect(() => {
+        if (select.current !== null) {
+            console.log(select.current.value)
+            select.current.value === 'priceasc' && sortAsc()
+            select.current.value === 'pricedesc' && sortDesc()
+        }
+    }, [products.length])
 
 
     return (
         <div className={styles.container}>
             <div className={styles.toprow}>
                 <p className={styles.results}>Showing all {products?.length} results</p>
-                <select className={styles.select} onChange={(e) => handleChange(e)}>
-                    <option value="pricedesc">price descending</option>
+                <select ref={select} className={styles.select} onChange={(e) => handleChange(e)}>
                     <option value="priceasc">price ascending</option>
+                    <option value="pricedesc">price descending</option>
                 </select>
             </div>
             <div className={styles.products}>
