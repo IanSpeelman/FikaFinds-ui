3b90d3af237d23cbd6f03482d26d59972f5a2912
Ian Speelman <ianspeelman@gmail.com>
2024-11-12 14:55:57 +0100

    correctly show order module
    calculate total price for order

--- src/Components/OrderProduct/index.tsx
+++ src/Components/OrderProduct/index.tsx
@@ -1,21 +1,24 @@
 import styles from './index.module.css'
-import { cartItemType } from '../../utils/types'
+import { cartItemType, orderProductResponse } from '../../utils/types'
 
 
 type OrderProductProps = {
     item: cartItemType,
+    order: orderProductResponse,
 }
 
 
 
-export default function OrderProduct({ item }: OrderProductProps) {
+export default function OrderProduct({ item, order }: OrderProductProps) {
+
+    console.log(order)
 
     return (
         <div className={styles.container}>
             <img className={styles.image} src={item.product.image !== 'test' ? item.product.image : 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQhik6ntKzUrb7GbmRt0laNt4TDv7p8sdfywfGt9JLMf_ZDrvuJWV6o_v4yFe42PP-luPrrAe-jl7Kg9IYPsLVEbHDvViAcW7CnyQeUWkF52g_NoB7o0dU68g'} alt={item.product.name} />
             <div className={styles.middle}>
                 <p className={styles.product}>{item.product.name}</p>
-                <p className={styles.amount}>{item.amount} x {item.product.price} SEK</p>
+                <p className={styles.amount}>{item.amount} x {order.price} SEK</p>
             </div>
         </div>
     )
--- src/Pages/Profile/Components/Order.tsx
+++ src/Pages/Profile/Components/Order.tsx
@@ -1,4 +1,4 @@
-import { orderResponse, cartItemType } from "../../../utils/types"
+import { orderResponse, cartItemType, orderProductResponse } from "../../../utils/types"
 import OrderProduct from '../../../Components/OrderProduct'
 import styles from './Order.module.css'
 import { useState, useEffect } from 'react'
@@ -10,40 +10,38 @@ type OrderProps = {
 export default function Order({ order }: OrderProps) {
     const [expanded, setExpanded] = useState(false)
     const [products, setProducts] = useState<cartItemType[] | []>([])
+    const [total, setTotal] = useState(0)
 
     useEffect(() => {
+        if (order.OrderProducts.length) {
+            setTotal(order.OrderProducts.reduce((acc, product) => acc += (product.amount * product.price), 0))
+        }
+        let tmp: orderProductResponse[] = []
         async function getData() {
             order.OrderProducts.forEach(async product => {
                 const response = await fetch(`${host}/products/${product.product}`)
-                if (response.ok) {
-                    const data = await response.json()
-                    setProducts([...products, { amount: data.amount, product: data }])
-                }
-            });
+                const data = await response.json()
+                tmp.push({ ...product, product: data[0] })
+            })
+            setProducts(tmp)
         }
         getData()
-
-
-
     }, [order])
 
     function handleClick() {
         setExpanded(!expanded)
     }
 
-    console.log('order', order.OrderProducts)
-    console.log('products', products)
-
     return (
         <div className={styles.container}>
             <div className={styles.order}>
                 <p className={styles.text}>order #{order.id}</p>
                 <p className={styles.text}>status: {'processing'}</p>
-                <p className={styles.text}>total: {'900SKEk'}</p>
+                <p className={styles.text}>total: {total}</p>
                 <button className={styles.button} onClick={handleClick}>view order</button>
             </div >
-            <div className={`${!expanded && styles.hidden}`}>
-                {products.length > 0 && products.map(product => <OrderProduct key={product.product.id} item={product} />)}
+            <div className={`${!expanded && styles.hidden} ${styles.products}`}>
+                {products.length > 0 && products.map((product, i) => <OrderProduct key={product.product.id} order={order.OrderProducts[i]} item={product} />)}
             </div>
         </div>
     )
